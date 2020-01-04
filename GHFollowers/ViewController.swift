a84beedc60b8c52e277155d35d64df1db83cf03c
Dmitriy Voronin <devoronin@gmail.com>
2020-01-04 11:45:49 +0300

    initial commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Dmitrii Voronin on 04.01.2020.
+//  Copyright © 2020 Mithfor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
