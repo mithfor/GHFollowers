49470f516bdf6edd43b72f8cac613b039526f286
Dmitriy Voronin <devoronin@gmail.com>
2020-01-04 12:58:47 +0300

    App Navigation Setup - UITabBarController & UI NavigationController

---
+++ GHFollowers/FavoritesListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListViewController.swift
+//  GHFollowers
+//
+//  Created by Dmitrii Voronin on 04.01.2020.
+//  Copyright © 2020 Mithfor. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+}
