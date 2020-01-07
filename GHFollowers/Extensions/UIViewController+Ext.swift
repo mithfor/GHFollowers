56b67dfe1b0a25bd9dd5e2c66e0967c58ba949e3
Dmitriy Voronin <devoronin@gmail.com>
2020-01-07 23:00:21 +0300

    Project organization and Architecture

---
+++ GHFollowers/Extensions/UIViewController+Ext.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController+Ext.swift
+//  GHFollowers
+//
+//  Created by Dmitrii Voronin on 07.01.2020.
+//  Copyright © 2020 Mithfor. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertViewController(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
+}
--- GHFollowers/Screens/SearchViewController.swift
+++ GHFollowers/Screens/SearchViewController.swift
@@ -41,7 +41,7 @@ class SearchViewController: UIViewController {
     @objc func pushFollowerListViewController() {
         
         guard isUsernameEntered else {
-            print("No username")
+            presentGFAlertOnMainThread(title: "Empty Username", message: "Please enter a username. We need to know who to look for 😀.", buttonTitle: "Ok")
             return
         }
         
