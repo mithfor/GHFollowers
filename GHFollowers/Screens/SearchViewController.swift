e42f6e60851cb3e515f86b0c650a0b65e93dc559
Dmitriy Voronin <devoronin@gmail.com>
2020-01-08 23:35:58 +0300

    Adding SearchViewController constants

--- GHFollowers/Screens/SearchViewController.swift
+++ GHFollowers/Screens/SearchViewController.swift
@@ -61,10 +61,10 @@ class SearchViewController: UIViewController {
         logoImageView.image = UIImage(named: "gh-logo")!
         
         NSLayoutConstraint.activate([
-            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: Constants.logoTopConstaraint),
             logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            logoImageView.heightAnchor.constraint(equalToConstant: 200),
-            logoImageView.widthAnchor.constraint(equalToConstant: 200),
+            logoImageView.heightAnchor.constraint(equalToConstant: Constants.logoSideSize),
+            logoImageView.widthAnchor.constraint(equalToConstant: Constants.logoSideSize),
         ])
     }
     
@@ -74,10 +74,13 @@ class SearchViewController: UIViewController {
         usernameTextField.delegate = self
         
         NSLayoutConstraint.activate([
-            usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
-            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
-            usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
-            usernameTextField.heightAnchor.constraint(equalToConstant: 50)
+            usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor,
+                                                   constant: Constants.padding),
+            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor,
+                                                       constant: Constants.padding),
+            usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor,
+                                                        constant: -Constants.padding),
+            usernameTextField.heightAnchor.constraint(equalToConstant: Constants.padding)
         ])
     }
     
@@ -86,13 +89,16 @@ class SearchViewController: UIViewController {
         callToActionButton.addTarget(self, action: #selector(pushFollowerListViewController), for: .touchUpInside)
         
         NSLayoutConstraint.activate([
-            callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
-            callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
-            callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
-            callToActionButton.heightAnchor.constraint(equalToConstant: 50)
+            callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,
+                                                       constant: -Constants.padding),
+            callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor,
+                                                        constant: Constants.padding),
+            callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor,
+                                                         constant: -Constants.padding),
+            callToActionButton.heightAnchor.constraint(equalToConstant: Constants.padding)
         ])
     }
-}
+ }
  
  extension SearchViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -100,3 +106,13 @@ class SearchViewController: UIViewController {
         return true
     }
  }
+ 
+ extension SearchViewController {
+    enum Constants {
+        static let padding: CGFloat = 50
+        static let logoSideSize: CGFloat = 150
+        static let logoTopConstaraint: CGFloat = 80
+    }
+ }
+
+ 
