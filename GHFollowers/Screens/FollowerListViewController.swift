c97479f919f34562d1d912d8b9337d0867eca2fc
Dmitriy Voronin <devoronin@gmail.com>
2020-01-26 21:41:01 +0300

    Scrolling to zero issue was fixed

--- GHFollowers/Screens/FollowerListViewController.swift
+++ GHFollowers/Screens/FollowerListViewController.swift
@@ -170,7 +170,8 @@ extension FollowerListViewController: FollowersListViewControllerDelegate {
         page            = 1
         followers.removeAll()
         filteredFollowers.removeAll()
-        collectionView.setContentOffset(.zero, animated: true)
+        //collectionView.setContentOffset(.zero, animated: true)
+        collectionView.scrollToItem(at: IndexPath(row: .zero, section: .zero), at: .top, animated: true)
         getFollowers(username: username, page: page)
     }
 }
